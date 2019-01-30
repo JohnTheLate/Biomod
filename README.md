dbfd6bcdf9160a9fcd6fffca6b099f4a938ecf42
JohnTheLate <aokhbusiness@gmx.de>
2019-01-30 18:33:00 +0100

    Additional element effects: Water breathing (Water, Stone), Air: jump fixed & ladder climb speed added, Fire: Fire harvest is WIP, Ice: More damage from lava

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 This is intended to become a Bionicle-inspired (Mata Nui/2001 only for the time being) mod to be used alongside NuiCraft by Eastonium. The player characters are more intended to have the Matoran/Tohunga role than that of a Toa.
 
-Features in the works:
-Elemental system:
+**Features in the works:**
+*Elemental system:*
 Pick 1 of 6 elements when your character joins a world the first time, granting some special effects, these being: Fire, Air, Earth, Ice, Water, Stone
 
-Bionicle tools:
+*Bionicle tools:*
 The 6* original Toas' and Turagas' weapons and staffs with unique effects - some more powerful than the others, but that's the way it is.
 *no Pohatu shoes yet
 
-Legacy content:
+*Legacy content:*
 Meteor ore and some other tutorial mod related things.
--- src/main/java/john/mod/BioEventSubscriber.java
+++ src/main/java/john/mod/BioEventSubscriber.java
@@ -6,19 +6,24 @@ import john.mod.util.Provider;
 import john.mod.util.interfaces.IElementHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.MoverType;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.FoodStats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -71,7 +76,7 @@ public class BioEventSubscriber {
 	public void playerLogin(PlayerEvent.PlayerLoggedInEvent event)
 	{
 		System.out.println("Event active: playerLogin");
-		getHandler(event.player).setElement(BioElements.WATER);
+		getHandler(event.player).setElement(BioElements.STONE);
 		BioElements element = getHandler(event.player).getElement();
 		final double defaultHealth = 20.D;
 		final float defaultWalkSpeed = 0.1F;
@@ -100,19 +105,20 @@ public class BioEventSubscriber {
 				break;
 		}
 
-		//getHandler(Minecraft.getMinecraft().player).setElement(BioElements.AIR);
+		//WaitForPlayerInitThread waitingThread = new WaitForPlayerInitThread(element);
+		//waitingThread.run();
 	}
 
 	@SubscribeEvent
-	public void livingDamageEventSub(LivingDamageEvent event)
+	public void livingDamageEventSub(LivingDamageEvent event)  //this method checks player elements and damage types to determine whether to affect the damage received
 	{
 		System.out.println("Event active: livingDamageEventSub");
 		if (event.getEntity() instanceof EntityPlayer)
 		{
 			System.out.println("Entity is EntityPlayer");
-			if (getHandler(event.getEntity()).getElement() == BioElements.FIRE) // check für BioElements.FIRE
+			if (getHandler(event.getEntity()).getElement() == BioElements.FIRE) //Fire receives reduced lava damage and none from fire at all
 			{
-				System.out.println("Check for Element succeeded");
+				//System.out.println("Check for Element succeeded");
 				if (event.getSource() == DamageSource.ON_FIRE || event.getSource() == DamageSource.IN_FIRE)
 				{
 					System.out.println("Fire damage nullified");
@@ -125,12 +131,20 @@ public class BioEventSubscriber {
 					event.setAmount(event.getAmount() * 0.5F);
 				}
 			}
-			else if (getHandler(event.getEntity()).getElement() == BioElements.AIR && event.getSource() == DamageSource.FALL)
+			else if (getHandler(event.getEntity()).getElement() == BioElements.ICE) //Ice receives extra lava damage
+			{
+				if (event.getSource() == DamageSource.LAVA)
+				{
+					System.out.println("Lava damage increased");
+					event.setAmount(event.getAmount() + 0.5F);
+				}
+			}
+			else if (getHandler(event.getEntity()).getElement() == BioElements.AIR && event.getSource() == DamageSource.FALL) //Air receives reduced fall damage
 			{
 				System.out.println("Fall damage halved");
 				event.setAmount(event.getAmount() * 0.5F);
 			}
-			else if (getHandler(event.getEntity()).getElement() == BioElements.WATER && event.getSource() == DamageSource.DROWN)
+			else if (getHandler(event.getEntity()).getElement() == BioElements.WATER && event.getSource() == DamageSource.DROWN) //Water receives reduced drown damage
 			{
 				System.out.println("Drown damage halved");
 				event.setAmount(event.getAmount() * 0.5F);
@@ -148,38 +162,22 @@ public class BioEventSubscriber {
 
 			EntityPlayer evPlayer = (EntityPlayer)event.getEntity();
 
-			//if (evPlayer.world.isRemote)
-			//{
-			/*if (getHandler(event.getEntity()).getElement() == BioElements.AIR) // check für BioElements.AIR
-			{
-				System.out.println("Element is AIR, adjusting Jump");
-				event.getEntity().motionY += 3.0D;
-				((EntityPlayer) event.getEntity()).getFoodStats().setFoodLevel(18);
-
-				event.getEntity().motionX *= 5.2D;
-				event.getEntity().motionZ *= 5.2D;
-			}*/
-
 			System.out.println("Element: " + getHandler(evPlayer).getElement());
 			System.out.println("Element: " + getHandler(Minecraft.getMinecraft().player).getElement());
 
-				Boolean abc = getHandler(evPlayer).getElement() == BioElements.AIR;
-
-				if (abc) // check für BioElements.AIR
-				{
-					System.out.println("Element is AIR, adjusting Jump");
-					evPlayer.motionX += 1.0D;
-				}
-				else
-				{
-					System.out.println("Element is not air here");
-				}
-
-			//}
+			if (getHandler(evPlayer).getElement() == BioElements.AIR) // check für BioElements.AIR
+			{
+				System.out.println("Element is AIR, adjusting Jump");
+				evPlayer.motionY += 0.1D;
+			}
+			else
+			{
+				System.out.println("Element is not air here");
+			}
 		}
 	}
 
-/*	@SubscribeEvent
+	@SubscribeEvent
 	public void landingEvent(LivingFallEvent event)
 	{
 		if (event.getEntity() instanceof EntityPlayer)
@@ -191,19 +189,62 @@ public class BioEventSubscriber {
 				event.getEntity().fallDistance *= 0.5;
 			}
 		}
-	}*/
+	}
+
+	@SubscribeEvent
+	public void leftClickBlock(PlayerInteractEvent.LeftClickBlock event)
+	{
+		EntityPlayer evPlayer = event.getEntityPlayer();
+
+		if (getHandler(evPlayer).getElement() == BioElements.FIRE)
+		{
+			System.out.println("LCB event running");
+			BlockPos pos = event.getPos().offset(event.getFace());
+			if (event.getWorld().getBlockState(pos).getBlock() == Blocks.FIRE)
+			{
+				System.out.println("drop Fire");
+
+				if (evPlayer.getHeldItem(EnumHand.MAIN_HAND).isEmpty())
+				{
+					evPlayer.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(Blocks.FIRE)); //TODO Use custom fire item and also use that for crafting recipes (chainmail, Ta-Koro guard staff)
+				}
+				else if (!evPlayer.inventory.addItemStackToInventory(new ItemStack(Blocks.FIRE)))
+				{
+					evPlayer.dropItem(new ItemStack(Blocks.FIRE), false);
+				}
+			}
+		}
+	}
 
 
 	@SubscribeEvent
 	public void playerTick(TickEvent.PlayerTickEvent event)
 	{
-		if (event.player.getAir() < 299 && getHandler(event.player).getElement() == BioElements.WATER)
+		EntityPlayer evPlayer = event.player;
+		if (evPlayer.getAir() < 299)
 		{
-			if (event.player.ticksExisted % 2 == 0)
+			if (getHandler(evPlayer).getElement() == BioElements.WATER && evPlayer.ticksExisted % 2 == 0) //Water can breathe for longer
 			{
-				event.player.setAir(event.player.getAir() + 1);
+				evPlayer.setAir(evPlayer.getAir() + 1);
+			}
+			else if (getHandler(evPlayer).getElement() == BioElements.STONE && evPlayer.getAir() % 10 == 0) //Stone can breathe for shorter
+			{
+				evPlayer.setAir(evPlayer.getAir() - 2);
 			}
 			//event.player.getFoodStats().addExhaustion(1.0F);
+		}
+
+		if (evPlayer.isOnLadder() && getHandler(evPlayer).getElement() == BioElements.AIR) //Air is supposed to climb faster
+		{
+			if (evPlayer.motionY <= -0.15D)
+			{
+				evPlayer.move(MoverType.SELF, 0,-0.15,0); //faster going down
+			}
+
+			if (evPlayer.collidedHorizontally)
+			{
+				evPlayer.motionY = 0.4D;
+			}
 		}
 	}
 
